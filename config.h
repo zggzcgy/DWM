0331328df2238d66cccd592aeacfcc5564cb052a
Mr.chen <zggzcgy@gmail.com>
2019-02-07 16:39:38 +0800

    fix Both terminals start at the same time.

--- config.h
+++ config.h
@@ -76,8 +76,6 @@ static const char *scrot[]     = { "scrot", "--exec", "mv $f ~/pictures", "%F_%H
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
