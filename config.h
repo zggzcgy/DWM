732080b822c9e7b113dd6efebbff9341af12f532
Mr.chen <zggzcgy@gmail.com>
2019-02-07 16:24:09 +0800

    Update dwm version to 6.2

--- config.h
+++ config.h
@@ -1,26 +1,27 @@
 /* See LICENSE file for copyright and license details. */
-#include <X11/XF86keysym.h>
+#include <X11/XF86keysym.h>	/* Portable computer extended function key support */
 
 /* appearance */
-static const char *fonts[] = {
-	"WenQuanYi Micro Hei:size=10"
-/*
-	"VL Gothic:size=41.5"
-	"monospace:size=11"
-	"Sans:size=10.5"
-*/
-};
-static const char dmenufont[]       = "monospace:size=10";
-static const char normbordercolor[] = "#444444";
-static const char normbgcolor[]     = "#222222";
-static const char normfgcolor[]     = "#bbbbbb";
-static const char selbordercolor[]  = "#005577";
-static const char selbgcolor[]      = "#005577";
-static const char selfgcolor[]      = "#eeeeee";
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { 
+	"WenQuanYi Micro Hei:size=10",
+	"VL Gothic:size=41.5",
+	"monospace:size=11"
+};
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
 
 /* tagging */
 static const char *tags[] = { "XTE", "WEB", "DOC", "AOU", "IMG", "GEM", "FIE", "VOD", "OTR" };
@@ -31,11 +32,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Firefox",	NULL,       NULL,       1 << 1,		0,		-1 },
-	{ "Wps",	NULL,       NULL,	1 << 2,		0,		-1 },
-	{ "Gimp",	NULL,       NULL,       1 << 4,		1,		-1 },
-	{ "Steam",	NULL,       NULL,       1 << 5,		1,		-1 },
-	{ "Vmware",	NULL,       NULL,       1 << 6,		0,		-1 },
+	{ "Firefox",    NULL,       NULL,       1 << 1,         0,              -1 },
+	{ "Wps",        NULL,       NULL,       1 << 2,         0,              -1 },
+	{ "Gimp",       NULL,       NULL,       1 << 4,         1,              -1 },
+	{ "Steam",      NULL,       NULL,       1 << 5,         1,              -1 },
+	{ "Vmware",     NULL,       NULL,       1 << 6,         0,              -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -63,23 +66,18 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]	= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]	= { "xterm", NULL };
-static const char *upvol[]	= { "amixer", "set", "Master", "2+", NULL };
-static const char *downvol[]	= { "amixer", "set", "Master", "2-", NULL };
-static const char *upback[]	= { "xbacklight", "-inc", "2", NULL };
-static const char *downback[]	= { "xbacklight", "-dec", "2", NULL };
-static const char *scrot[]	= { "scrot", "--exec", "mv $f ~/pictures", "%F_%H%M%S.png", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]   = { "xterm", NULL };
+static const char *upvol[]     = { "amixer", "set", "Master", "2+", NULL };
+static const char *downvol[]   = { "amixer", "set", "Master", "2-", NULL };
+static const char *upback[]    = { "xbacklight", "-inc", "2", NULL };
+static const char *downback[]  = { "xbacklight", "-dec", "2", NULL };
+static const char *scrot[]     = { "scrot", "--exec", "mv $f ~/pictures", "%F_%H%M%S.png", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,		spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return,	spawn,          {.v = termcmd } },
-	{ 0,		XF86XK_AudioLowerVolume,	spawn,			{.v = downvol } },
-	{ 0,		XF86XK_AudioRaiseVolume,	spawn,			{.v = upvol } },
-	{ 0,		XF86XK_MonBrightnessUp,		spawn,			{.v = upback } },
-	{ 0,		XF86XK_MonBrightnessDown,	spawn,			{.v = downback } },
-	{ 0,				XK_Print,	spawn,			{.v = scrot } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -101,6 +99,13 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ 0,                            XK_Print,  spawn,          {.v = scrot } },
+	{ 0,            XF86XK_AudioLowerVolume,   spawn,          {.v = downvol } },
+	{ 0,            XF86XK_AudioRaiseVolume,   spawn,          {.v = upvol } },
+	{ 0,            XF86XK_MonBrightnessUp,    spawn,          {.v = upback } },
+	{ 0,            XF86XK_MonBrightnessDown,  spawn,          {.v = downback } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -114,7 +119,7 @@ static Key keys[] = {
 };
 
 /* button definitions */
-/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
